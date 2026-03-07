db4aefb07045e868fb64440bc16381c99ce2a30d
cakeslayer00 <mainacc1838@gmail.com>
2026-03-07 15:25:01 +0500

    chore: creating pem files to resources directory

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 mkdir -p secrets/backend
 
 # Generate PKCS#8 private key directly
-openssl genpkey -algorithm RSA -out secrets/backend/private_key.pem -pkeyopt rsa_keygen_bits:2048
+openssl genpkey -algorithm RSA -out services/backend/src/main/resources/private_key.pem -pkeyopt rsa_keygen_bits:2048
 
 # Extract public key
-openssl rsa -in secrets/backend/private_key.pem -pubout -out secrets/backend/public_key.pem
+openssl rsa -in services/backend/src/main/resources/private_key.pem -pubout -out services/backend/src/main/resources/public_key.pem
 
 echo "Secrets generated successfully"
